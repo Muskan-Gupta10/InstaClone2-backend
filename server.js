080d7a6c7a73f6c03e6600fb852e6c5c0c64e5ad
Muskan-Gupta10 <125127176+Muskan-Gupta10@users.noreply.github.com>
2023-03-31 21:00:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv=require("dotenv")
 const cors=require("cors")
 app.use(express.json())
 app.use(cors({
-    origin:"https://instaclone-frontend-a3oh.onrender.com"
+    origin:"https://instaclone2-frontend.onrender.com"
 }))
 const PORT=process.env.PORT || 8080
 dotenv.config()
